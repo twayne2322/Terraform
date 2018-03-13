c3715903791698694d2b84a2a827ee2f3336da35
Thomas Horlacher <thomashorlacher@Thomass-MacBook-Pro-3.local>
2018-03-12 20:51:36 -0400

    Added stuff

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-40d28157"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -25,4 +26,8 @@ resource "aws_security_group" "instance" {
     to_port = 8080
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
 }
