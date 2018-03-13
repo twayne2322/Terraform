836132f74978d12d9e9da8a002a4c6317b4f1eda
Thomas Horlacher <thomashorlacher@Thomass-MacBook-Pro-3.local>
2018-03-12 20:44:21 -0400

    Added stuff

--- main.tf
+++ main.tf
@@ -5,7 +5,24 @@ resource "aws_instance" "example" {
   ami = "ami-40d28157"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
   tags {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "tearraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
