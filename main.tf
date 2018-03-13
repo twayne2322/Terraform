6b60f9caf6fe653d63a9c0c5eaddf97c3bac2cc1
Thomas Horlacher <thomashorlacher@Thomass-MacBook-Pro-3.local>
2018-03-12 21:01:03 -0400

    Added lifecyle

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "example" {
+resource "aws_launch_configuration" "example" {
   ami = "ami-40d28157"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
@@ -12,9 +12,14 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
+  lifecycle {
+    create_before_destroy = true
+  }
+
   tags {
     Name = "terraform-example"
   }
+  image_id = ""
 }
 
 resource "aws_security_group" "instance" {
